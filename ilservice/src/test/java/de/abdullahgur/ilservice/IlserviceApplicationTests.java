93832f29e88025188e07a6e23a8eb7054a172d7f
Abdullah Gür <14290100@ogrenci.ankara.edu.tr>
2022-06-11 12:45:10 +0300

    maven_spring_libraries_push

---
+++ ilservice/src/test/java/de/abdullahgur/ilservice/IlserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.abdullahgur.ilservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
